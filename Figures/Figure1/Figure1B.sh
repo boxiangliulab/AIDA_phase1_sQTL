4a647b71d99896a03a24ca7ab91ced0e469c16a2
Chi Tian <31667880+skywalker51616@users.noreply.github.com>
2024-07-30 16:45:25 +0800

    Add files via upload

--- Figures/Figure1/Figure1B.sh
+++ Figures/Figure1/Figure1B.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+mkdir -p heatmap
+cd heatmap
+
+samtools index -@ 16 ../mergeAll001.all_celltypes.R1.bam
+samtools index -@ 16 ../mergeAll001.all_celltypes.R2.bam
+
+bamCoverage -b ../mergeAll001.all_celltypes.R1.bam -o ../mergeAll001.all_celltypes.R1.bw --normalizeUsing RPKM -bs 5 -p 16 >log.bw1.txt 2>&1
+bamCoverage -b ../mergeAll001.all_celltypes.R2.bam -o ../mergeAll001.all_celltypes.R2.bw --normalizeUsing RPKM -bs 5 -p 16 >log.bw2.txt 2>&1
+
+
+# final version: expressed genes
+grep -f ../genelist_expressed.txt /ebs1/shared/data/reference/hg38/gencode.v32.annotation.gtf | less > gencode.v32.annotation.expr.gtf
+computeMatrix scale-regions \
+              -R gencode.v32.annotation.expr.gtf \
+              -S ../mergeAll001.all_celltypes.R1.bw ../mergeAll001.all_celltypes.R2.bw \
+              --metagene \
+              -o mergeAll001.all_celltypes.mat.final.gz --outFileSortedRegions sortedRegions.bed \
+              -b 1000 -a 1000 \
+              --sortUsing mean \
+              --skipZeros \
+              -p 16 >log.heatmap.matrix.final.txt 2>&1
+
+plotHeatmap -m mergeAll001.all_celltypes.mat.final.gz \
+            --samplesLabel Read1 Read2 \
+            --heatmapWidth 4 --heatmapHeight 9 \
+            --colorMap Blues Blues \
+            -out mergeAll001.all_celltypes.heatmap.final.pdf >log.heatmap.final.txt 2>&1
